61e02664d8b3c790381f1f931623355989e98161
pari1jay <129131428+pari1jay@users.noreply.github.com>
2023-12-05 14:53:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import sklearn
 
-import scikit-learn as sklearn
+#import scikit-learn as sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
